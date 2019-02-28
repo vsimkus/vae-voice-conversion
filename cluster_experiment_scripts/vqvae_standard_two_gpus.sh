68aeaaf257342e79ab20bdcb56c0f63bb26c9230
Vaidotas Simkus <simkus.vaidotas@gmail.com>
2019-02-28 01:29:43 +0000

    Reduced batch size, increased number of gpus to 2 for vqvae exp.

--- cluster_experiment_scripts/vqvae_standard_two_gpus.sh
+++ cluster_experiment_scripts/vqvae_standard_two_gpus.sh
@@ -2,7 +2,7 @@
 #SBATCH -N 1	  # nodes requested
 #SBATCH -n 1	  # tasks requested
 #SBATCH --partition=Standard
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:2
 #SBATCH --mem=12000  # memory in Mb
 #SBATCH --time=0-08:00:00
 
@@ -34,13 +34,14 @@ rsync -ua --progress /home/${STUDENT_ID}/data/ ${TMP}/data/
 unzip -u ${TMP}/data/processed_data.zip -d ${TMP}/data
 
 export DATASET_DIR=${TMP}/data/processed_data/
-# Activate the relevant virtual environment:
 
+# Activate the relevant virtual environment:
 source /home/${STUDENT_ID}/miniconda3/bin/activate mlp
 cd ..
+echo "Starting train_evaluate_vqvae.py"
 python train_evaluate_vqvae.py \
                 --use_gpu=True \
-                --gpu_id='0' \
+                --gpu_id='0,1' \
                 --experiment_name='vqvae_init_exp' \
                 --filepath_to_arguments_json_file='experiment_configs/vqvae_architecture.json' \
                 --dataset_root_path=${DATASET_DIR} 
