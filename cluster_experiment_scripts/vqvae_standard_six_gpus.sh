30b6bbe363d832d93bd1935c945bbaa4e866749b
Vaidotas Simkus <simkus.vaidotas@gmail.com>
2019-03-01 10:58:21 +0000

    Cleared-up the workspace.

--- cluster_experiment_scripts/vqvae_standard_six_gpus.sh
+++ cluster_experiment_scripts/vqvae_standard_six_gpus.sh
@@ -5,6 +5,7 @@
 #SBATCH --gres=gpu:6
 #SBATCH --mem=12000  # memory in Mb
 #SBATCH --time=0-08:00:00
+#SBATCH --exclude=ladonia[20,24]
 
 export CUDA_HOME=/opt/cuda-9.0.176.1/
 
@@ -38,12 +39,11 @@ export DATASET_DIR=${TMP}/data/
 # Activate the relevant virtual environment:
 source /home/${STUDENT_ID}/miniconda3/bin/activate mlp
 cd ..
-echo "Starting train_evaluate_vqvae.py"
+echo "Starting train_vqvae.py"
 # export PYTHONUNBUFFERED=TRUE # This allows to dump the log messages into stdout immediately
-python train_evaluate_vqvae.py \
-                --tuning_mode=True \
+python train_vqvae.py \
                 --use_gpu=True \
                 --gpu_id='0,1,2,3,4,5' \
-                --experiment_name='vqvae_init_exp_small' \
+                --experiment_name='vqvae_init_exp' \
                 --filepath_to_arguments_json_file='experiment_configs/vqvae_architecture.json' \
                 --dataset_root_path=${DATASET_DIR} 
