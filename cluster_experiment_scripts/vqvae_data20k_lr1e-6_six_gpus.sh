5a7a03c209b5504cf9e6b6ff4412a0d3b521ee65
Vaidotas Simkus <simkus.vaidotas@gmail.com>
2019-03-02 01:45:20 +0000

    Corrected node names.

--- cluster_experiment_scripts/vqvae_data20k_lr1e-6_six_gpus.sh
+++ cluster_experiment_scripts/vqvae_data20k_lr1e-6_six_gpus.sh
@@ -5,7 +5,7 @@
 #SBATCH --gres=gpu:6
 #SBATCH --mem=12000  # memory in Mb
 #SBATCH --time=0-08:00:00
-#SBATCH --exclude=ladonia[20,24]
+#SBATCH --exclude=landonia[20,24]
 
 export CUDA_HOME=/opt/cuda-9.0.176.1/
 
